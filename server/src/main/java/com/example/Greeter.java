a132be39ada6724f62e0e2059ec1eed97475a2dc
gitbalajee <47471702+gitbalajee@users.noreply.github.com>
2019-03-06 12:47:58 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,5 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
-  public String newGreet(){
-    return "Balajee Jha";
-  }
+  
 }
